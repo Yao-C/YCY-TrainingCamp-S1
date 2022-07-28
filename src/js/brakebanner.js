9b43ed7f91e723fbae969926aa8d7c5266521980
Yoyo <chengyao19920131@163.com>
2022-07-28 17:00:26 +0200

    2.添加自行车,使刹车与按钮交互

--- src/js/brakebanner.js
+++ src/js/brakebanner.js
@@ -4,7 +4,8 @@ class BrakeBanner {
 		this.app = new PIXI.Application({
 			width: window.innerWidth,
 			height: window.innerHeight,
-			backgroundColor: 0xff0000
+			backgroundColor: 0xffffff,
+			resizeTo: window
 		});
 		this.stage = this.app.stage;
 
@@ -30,13 +31,62 @@ class BrakeBanner {
 		let actionButton = this.createActionButton();
 
 		// 移动整个容器的位置到网页视野中
-		actionButton.x = actionButton.y = 400;
+		actionButton.x = 170;
+		actionButton.y = 230;
+
+		// 创建自行车容器
+		let bikeContainer = new PIXI.Container();
+		this.stage.addChild(bikeContainer);
+
+		// 车身尺寸大，缩放容器大小使图片在页面可见
+		bikeContainer.scale.x = bikeContainer.scale.y = 0.3;
+
+		let bikeImage = new PIXI.Sprite(this.loader.resources['brake_bike.png'].texture);
+		bikeContainer.addChild(bikeImage);
+
+		let bikeLeverImage = new PIXI.Sprite(this.loader.resources['brake_lever.png'].texture);
+		bikeContainer.addChild(bikeLeverImage);
+
+		// 设置刹车杆旋转的轴心，为之后刹车动效做准备
+		bikeLeverImage.pivot.x = 455;
+		bikeLeverImage.pivot.y = 455;
+
+		// 设置刹车杆位置到与车把手相连，没有尺寸可在ps中量一下
+		bikeLeverImage.x = 722;
+		bikeLeverImage.y = 900;
+
+		let bikeHandlerbarImage = new PIXI.Sprite(this.loader.resources['brake_handlerbar.png'].texture);
+		bikeContainer.addChild(bikeHandlerbarImage);
+
+
+		this.stage.addChild(actionButton);
+		actionButton.interactive = true; // 具备与用户交互的能力
+		actionButton.buttonMode = true;  // 鼠标移动到按钮会出现小手的图标
+
+		// 添加按钮事件，点击刹车，松开不刹
+		actionButton.on("mousedown", () => {
+			// bikeLeverImage.rotation = Math.PI / 180 * -30; // 按刹车效果
+			// 用gasp效果后，刹车更逼真
+			gsap.to(bikeLeverImage, { duration: 0.6, rotation: Math.PI / 180 * -30 });
+		})
+		actionButton.on("mouseup", () => {
+			// bikeLeverImage.rotation = 0; 
+			gsap.to(bikeLeverImage, { duration: 0.6, rotation: 0 });
+		})
+
+    // 使自行车根据窗口大小而调节，一直处于窗口右下角
+		let resize = () => {
+			bikeContainer.x = window.innerWidth - bikeContainer.width;
+			bikeContainer.y = window.innerHeight - bikeContainer.height;
+		}
+		window.addEventListener('resize', resize);
+		resize();
 	}
 
 	createActionButton() {
 		// 使用容器来管理动效按钮
 		let actionButton = new PIXI.Container();
-		this.stage.addChild(actionButton);
+		actionButton.scale.x = actionButton.scale.y = 0.4;
 
 		// 从加载器中取出按钮相关图片并放入容器中
 		let btnImage = new PIXI.Sprite(this.loader.resources['btn.png'].texture);
